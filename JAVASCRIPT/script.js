bab66381dd35c8db024e8d7ee230acaa6bc168b3
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-14 01:37:05 +0800

    feat(js):add basic hello world text and math operation

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,9 @@
+//Simple js programs
+console.log("Hello, kwan");
+console.log("I  am learning basic fundamentals of js to OOP");
+
+//basic  math operation
+let num1 = 20;
+let num2 = 8;
+let total = num1+num2;
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + total);
