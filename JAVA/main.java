5e33b86ffaaff5362c1f64e51736462e31851ae2
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-14 01:25:21 +0800

    fix java capital name of file

--- JAVA/main.java
+++ JAVA/main.java
@@ -1,10 +1,9 @@
-public class main{
+public class Main {
     public static void main(String[] args) {
         System.out.println("World, Hello !");
         System.out.println("I am currently relearning java fundamentals to OOP");
     }
 }
-
 
 
 
