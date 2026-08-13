babcd3176925d17bd8cec34e820b28f04dfc2be7
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-14 01:14:41 +0800

    add basic python greet message and math

---
+++ PYTHON/main.py
@@ -0,0 +1,7 @@
+#Simple Python Program
+name = "Ruperth"
+print(f"Hello, {name}! Welcome to Python programming.")
+#Basic math 
+num1 = 30
+num2 = 2
+print(f"The difference of {num1} and {num2} is {num1 - num2}.")
