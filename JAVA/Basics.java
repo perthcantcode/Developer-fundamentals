e94bf5a106df886a102e485d6ababbb5f1b37402
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-15 02:15:48 +0800

    fix main() nad age string to int syntax error

--- JAVA/Basics.java
+++ JAVA/Basics.java
@@ -6,7 +6,7 @@ public class Basics {
         
         //Variables diff data types
         String name;
-        String age;
+        int age;
 
         //Getting input from the user
         System.out.print("Enter your name: ");
@@ -15,15 +15,19 @@ public class Basics {
         age = scanner.nextInt();
 
         //Using the variables
-        System.out.print("Hello " + name + " you are " + age + " years old");
+        System.out.print("Hello " + name + " you are " + age + " years old \n");
 
         //Conditionals (if-else)
 
         if(age<=18){System.out.println("You are a minor");}
         else{System.out.println("You are an adult");}
-    }
 
     //Loops (for loops)
-    System.out.println("Coounting down from " + age + " to 1 :");
+    System.out.println("Counting down from " + age + " to 1 : ");
+    for(int i=age;  i>= 1; i--){
+        System.out.print(i);
+    }
 
+    scanner.close();
+    }
 }
