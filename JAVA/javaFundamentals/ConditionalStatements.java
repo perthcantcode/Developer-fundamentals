9e2389caa9f8d4bfee7127ff3fc9ff046ba6fe0c
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-22 00:59:21 +0800

    if else if conditions and one liner conditional way

--- JAVA/javaFundamentals/ConditionalStatements.java
+++ JAVA/javaFundamentals/ConditionalStatements.java
@@ -7,13 +7,24 @@ public class ConditionalStatements {
         System.out.print("Enter Age: ");
         int age = scan.nextInt();
 
+        int teen = 13;
+        int kid = 4;
         //int legalAge = 18; pwede if compare like (age >= legalAge)
         if(age >= 18){
             System.out.println("You have access!"); // if >18 lang di kasama 18
+        }else if(age >= teen ){
+            System.out.println("You need parent consent");
+        }else if(age >= kid){
+            System.out.println("You are a kid");
         }else{
-            System.out.println("Access Denied !");
+            System.out.println("You are a baby WTF bro");
         }
 
+        //can remove curly braces {} if one liner lang conditions 
+
+        
+
+        
 
     }
 }
