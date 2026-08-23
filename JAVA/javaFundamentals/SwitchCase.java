adbcb989b79f896928ce95867479016bb9988699
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-23 17:44:39 +0800

    grade through switch and default keyword

--- JAVA/javaFundamentals/SwitchCase.java
+++ JAVA/javaFundamentals/SwitchCase.java
@@ -5,6 +5,20 @@ public class SwitchCase {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        
+        char grade = 'A';
+
+        switch(grade){
+            case 'A' :
+                System.out.println("Outstanding");
+                break;
+            case 'B':
+                System.out.println("Excellent");
+                break;
+            case 'C':
+                System.out.println("Satisfaction");
+                break;
+            default:
+                System.out.println("Invalid Grade");
+        }
     }
 }
