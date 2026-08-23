71ca12672299d94d842a9ba7a32464815b1e0e5f
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-23 18:29:47 +0800

    add default

--- JAVA/javaFundamentals/SwitchCase.java
+++ JAVA/javaFundamentals/SwitchCase.java
@@ -56,51 +56,27 @@ public class SwitchCase {
         System.out.print("Enter Month : ");
         int month = sc.nextInt();
 
-        System.out.print("Enter Date: ");
+        System.out.print("Enter Date  : ");
         int date = sc.nextInt();
 
-        System.out.print("Enter Year: ");
+        System.out.print("Enter Year  : ");
         int year = sc.nextInt();
 
 
         switch(month){
-            case 1:
-                System.out.println("January " + date + ", " + year);
-                break;
-            case 2:
-                System.out.println("Febuary " + date + ", " + year);
-                break;
-            case 3:
-                System.out.println("March " + date + ", " + year);
-                break;
-            case 4:
-                System.out.println("April  " + date + ", " + year);
-                break;
-            case 5:
-                System.out.println(:"May " + date + ", " + year);
-                break;
-            case 6:
-                System.out.println("June" + date + ", " + year);
-                break;
-            case 7:
-                System.out.println("July " + date + ", " + year);
-                break;
-            case 8:
-                System.out.println("August " + date + " ," + year);
-                break;
-            case 9:
-                System.out.println("September " + date + ", " + year);
-                break;
-            case 10:
-                System.out.println("October " + date + ", " + year);
-                break;
-            case 11:
-                System.out.println("November " + date _+ ", " + year);
-                break;
-            case 12:
-                System.out.println("December " + date + ", " + year);
-                break;
-            
+            case 1:System.out.println("January " + date + ", " + year);break;
+            case 2:System.out.println("Febuary " + date + ", " + year);break;
+            case 3:System.out.println("March " + date + ", " + year);break;
+            case 4:System.out.println("April  " + date + ", " + year);break;
+            case 5:System.out.println("May " + date + ", " + year);break;
+            case 6:System.out.println("June" + date + ", " + year);break;
+            case 7:System.out.println("July " + date + ", " + year);break;
+            case 8:System.out.println("August " + date + " ," + year);break;
+            case 9:System.out.println("September " + date + ", " + year);break;
+            case 10:System.out.println("October " + date + ", " + year);break;
+            case 11:System.out.println("November " + date + ", " + year);break;
+            case 12:System.out.println("December " + date + ", " + year);break;
+            default:System.out.println("Invalid Month Date");
         }
 
     }
