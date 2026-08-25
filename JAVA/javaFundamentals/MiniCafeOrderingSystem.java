7fbde792f685f7752c1949b9fc517bc3515a176c
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-25 23:39:30 +0800

    add double money var and boolean isOpen* corection var

--- JAVA/javaFundamentals/MiniCafeOrderingSystem.java
+++ JAVA/javaFundamentals/MiniCafeOrderingSystem.java
@@ -3,7 +3,7 @@ package javaFundamentals;
 public class MiniCafeOrderingSystem {
     public static void main(String[] args) {
         double money = 10.00;
-
+        boolean isOpen = true;
 
     }
 }
