41ca7fee95ada890cc7547b1f2673e829d999a8a
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-25 23:39:54 +0800

    add scanner for user input

--- JAVA/javaFundamentals/MiniCafeOrderingSystem.java
+++ JAVA/javaFundamentals/MiniCafeOrderingSystem.java
@@ -1,9 +1,12 @@
 package javaFundamentals;
 
+import java.util.Scanner;
+
 public class MiniCafeOrderingSystem {
     public static void main(String[] args) {
         double money = 10.00;
         boolean isOpen = true;
 
+        Scanner scanner = new Scanner(System.in); 
     }
 }
