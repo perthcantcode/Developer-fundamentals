6233cc209ac384df0db083f6d1cdc26ed7f0f058
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-25 23:41:04 +0800

    add console menu display

--- JAVA/javaFundamentals/MiniCafeOrderingSystem.java
+++ JAVA/javaFundamentals/MiniCafeOrderingSystem.java
@@ -12,7 +12,8 @@ public class MiniCafeOrderingSystem {
         System.out.println("Welcome to the Mini Cafe!");
 
         while(isOpen){
-            
+            System.out.println("\nMenu: 1. Coffee ($3) | 2. Snack | 3. Check Wallet | 4. Leave");
+            System.out.print("Enter your choice: ");
         }
     }
 }
