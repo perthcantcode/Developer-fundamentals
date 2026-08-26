bc1cfb0a79b077038c1571deadb1f3618392a4fd
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-26 23:27:43 +0800

    SQL4: Create students table with variables and datatypes

--- SQL/practice.sql
+++ SQL/practice.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE school_db;
+
 USE school_db;
+
 DROP DATABASE IF EXISTS test_db;
+
+CREATE TABLE  students (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name(50),
+    age INT
+);
+
