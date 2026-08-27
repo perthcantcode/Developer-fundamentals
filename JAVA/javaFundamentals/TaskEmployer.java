a51f1a85f07423d77b3507673b5c22eb6dc93ea8
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-27 23:41:23 +0800

    Task Employer user input

---
+++ JAVA/javaFundamentals/TaskEmployer.java
@@ -0,0 +1,12 @@
+package javaFundamentals;
+import java.util.Scanner;
+public class TaskEmployer {
+    public static void main(String[] args) {
+        System.out.println("=== Company Task List ===");
+
+
+
+    }
+    
+
+}
