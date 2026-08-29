2fbd366d9a4659e474dfb682fa4e56635858cc1c
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-29 23:37:24 +0800

    CREATE TABLE

--- SQL/prac.sql
+++ SQL/prac.sql
@@ -1,2 +1,9 @@
 -- creates a new empty database.
 CREATE DATABASE company_db;
+
+-- 2. CREATE TABLE & CONSTRAINTS
+-- Creates tables with defined structures and rules (PRIMARY KEY, NOT NULL).
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50) NOT NULL
+);
