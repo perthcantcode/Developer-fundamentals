1454ffe21e7be9b1dff86ffa9b5ad96659a4657f
perthcantcode <ruperthayvesina1323@gmail.com>
2026-08-29 23:38:02 +0800

    finalize 2 tables, one for departmens and one for employees

--- SQL/prac.sql
+++ SQL/prac.sql
@@ -7,3 +7,15 @@ CREATE TABLE departments (
     dept_id INT PRIMARY KEY,
     dept_name VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    hire_date DATE,
+    salary DECIMAL(10, 2),
+    dept_id INT,
+    -- 3. FOREIGN KEY CONSTRAINT
+    -- Links this table to the 'departments' table.
+    FOREIGN KEY (dept_id) REFERENCES departments(dept_id)
+);
